a251dec0365178100db1e57d66e23148139232a2
Salih Muhammed <lr0@gmx.com>
2024-03-23 00:33:18 +0200

    Upload Go example of dynamic webpage

---
+++ ims.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"html/template" // New import
+	"log"           // New import
+	"net/http"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Include the navigation partial in the template files.
+	files := []string{
+		"./pages/base.tmpl",
+		"./pages/partials/nav.tmpl",
+		"./pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.ListenAndServe(":8080", nil)
+}
