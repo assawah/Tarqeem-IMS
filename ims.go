e9ce2bd05336f6437b4e0bf8f1a15f91a97cb2f3
Salih Muhammed <lr0@gmx.com>
2024-03-23 04:33:16 +0200

    Auto update with debug

--- ims.go
+++ ims.go
@@ -33,17 +33,32 @@ func getTemplates() (*template.Template, error) {
 //go:embed public/*
 var public embed.FS
 
+const debug = true
+
+var executor TemplateExecutor
+
 func main() {
 	ts, err := getTemplates()
 	if err != nil {
 		log.Fatal(err)
+	}
+	if debug {
+
+		files, err := getFSFilesRecursively(&views, "pages")
+		if err != nil {
+			log.Fatal(err)
+		}
+		executor = DebugTemplateExecutor{files}
+
+	} else {
+		executor = ts
 	}
 
 	staticHandler := http.FileServer(http.FS(public))
 	http.Handle("/static/", http.StripPrefix("/static/", staticHandler))
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := ts.ExecuteTemplate(w, "base", nil)
+		err := executor.ExecuteTemplate(w, "base", nil)
 		if err != nil {
 			log.Println(err.Error())
 			http.Error(w, "Internal Server Error", 500)
