51ebc60a41b63d840e2811fd1c95519c7512b404
Mostafa Rady <mr2076@fayoum.edu.eg>
2024-03-26 23:40:44 +0200

    Add dashboard page

---
+++ public/main.js
@@ -0,0 +1,36 @@
+const filters = document.querySelectorAll(".btn-check")
+
+
+filters.forEach((filter) => {
+    filter.addEventListener('click', () => {
+        if (filter.checked == true) {
+            console.log("checked")
+        }
+        else {
+            console.log("unchecked")
+        }
+    })
+})
+
+
+
+let originalArray = [1, 2, 3];
+
+// Create a proxy for the array
+let arrayProxy = new Proxy(originalArray, {
+    set:function(target, property, value) {
+        // Intercept array property assignment
+        target[property] = value;
+
+        // Trigger some action when array changes
+        console.log('Array changed:', target);
+
+        // You can perform any custom action here
+        // For example, you can call a function, update UI, etc.
+    }
+});
+
+// Now, whenever you modify arrayProxy, it will trigger the set trap and log the changes
+arrayProxy.push(4); // This will log 'Array changed: [1, 2, 3, 4]'
+
+console.log("test js file ")
