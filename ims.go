20794b53725e1a56eac58bcf56967753e1dde82c
Salih Muhammed <lr0@gmx.com>
2024-03-28 12:40:02 +0200

    Update web server

--- ims.go
+++ ims.go
@@ -2,27 +2,27 @@ package main
 
 import (
 	"embed"
+	"io"
 	"log"
 	"net/http"
 	"text/template"
 
-	"github.com/tarqeem/template/utl"
-)
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
-//go:embed public/*
-var public embed.FS
+	. "github.com/tarqeem/template/utl"
+)
 
 //go:embed pages/*
 var views embed.FS
 
 const debug = true
 
-var executor utl.TemplateExecutor
+var executor TemplateExecutor
 
 func main() {
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
-	utl.Views = views
-	utl.TemplateFuncs = template.FuncMap{
+	Views = views
+	TemplateFuncs = template.FuncMap{
 		"message": func(k string) string {
 			if val, ok := English[k]; ok {
 				return val
@@ -33,8 +33,7 @@ func main() {
 
 		},
 	}
-
-	ts, err := utl.GetTemplates()
+	ts, err := GetTemplates()
 
 	if err != nil {
 		log.Fatal(err)
@@ -44,19 +43,40 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		executor = utl.DebugTemplateExecutor{}
+		executor = DebugTemplateExecutor{}
 
 	} else {
 		executor = ts
 	}
 
-	staticHandler := http.FileServer(http.FS(public))
-	http.Handle("/static/", http.StripPrefix("/static/", staticHandler))
+	e := echo.New()
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.Static("static"))
+	e.Renderer = &etmplt{templates: ts}
 
-	// routes
+	e.GET("/", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "login", nil)
+	})
 
-	http.HandleFunc("/", NewHandler("login", nil))
-	http.HandleFunc("/home", NewHandler("dashboard", nil))
+	e.GET("/dashboard", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "dashboard", nil)
+	})
 
-	http.ListenAndServe(":8080", nil)
+	e.GET("/register", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "register", nil)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+type etmplt struct {
+	templates *template.Template
+}
+
+func (t *etmplt) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	if debug {
+		return executor.ExecuteTemplate(w, name, data)
+	}
+	return t.templates.ExecuteTemplate(w, name, data)
 }
