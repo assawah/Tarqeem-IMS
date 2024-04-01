d893c294fcb7a9e0dd6a11b33ee9552e6e34de3d
Mostafa Rady <mr2076@fayoum.edu.eg>
2024-04-01 02:15:38 +0200

    complete notification sidebar

--- static/main.js
+++ static/main.js
@@ -1,12 +1,21 @@
 
 
-const Notifications = document.getElementById("icon")
-const NotificationIcon = document.getElementById("notifications")
+const NotificationIcon = document.getElementById("icon")
+const Notifications = document.getElementById("notifications")
+const cancel_btn = document.getElementById("cancel_btn")
 
 NotificationIcon.addEventListener("click", () => {
-    console.log("clicked")
     Notifications.classList.add("show")
     NotificationIcon.classList.add("hidden")
 
 })
 
+
+cancel_btn.addEventListener("click", () => {
+    Notifications.classList.remove("show")
+    NotificationIcon.classList.remove("hidden")
+
+})
+
+
+console.log(NotificationIcon)
